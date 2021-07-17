0d0c3e7f996b5bbd14bdf6e5985586fc166d4818
Christian Swinehart <drafting@samizdat.co>
2021-07-17 10:56:11 -0400

    Update testtext.js

--- testtext.js
+++ testtext.js
@@ -4,7 +4,7 @@ let WIDTH = 512,
     canvas = new Canvas(WIDTH, HEIGHT),
     ctx = canvas.getContext("2d")
 
-// ctx.font = '80px Arial'
+ctx.font = '80px Arial'
 ctx.fillText('ABC abc 123', 10, 200)
 
 ctx.fillRect(10, 220, WIDTH-20, 10)
