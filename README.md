ca0a49d0cf6c3ec90fa14b14732cd1699f343ad3
Christian Swinehart <drafting@samizdat.co>
2021-09-20 19:56:29 -0400

    use skia-safe directly

--- README.md
+++
@@ -1,6 +0,0 @@
-#### Staging Area for Future Releases
-> This place is not a place of honor  
-> no highly esteemed deed is commemorated here  
-> nothing valued is here  
-
-Try going to the [Skia Canvas respository](https://github.com/samizdatco/skia-canvas) to find the actual project.
--- testtext.js
+++
@@ -1,9 +0,0 @@
-const {Canvas, FontLibrary} = require('./lib')
-let WIDTH = 512, HEIGHT = 128,
-    canvas = new Canvas(WIDTH, HEIGHT),
-    ctx = canvas.getContext("2d");
-
-ctx.font = '80px Arial'
-ctx.fillText('ABC abc 123', 10, 80)
-ctx.fillRect(10, 100, WIDTH-20, 10)
-canvas.saveAs('abc.png', {density:2})
