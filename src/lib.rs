5c9ec75ff94b03008a6dfd36110572773769cfa6
Christian Swinehart <drafting@samizdat.co>
2021-09-20 18:02:32 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,8 @@ lazy_static! {
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
+  use skia_safe::shaper::icu;
+  icu::init();
 
   // -- Image -------------------------------------------------------------------------------------
 
