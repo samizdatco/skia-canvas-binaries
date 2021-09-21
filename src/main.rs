41d4e2d08a9ef57fe0ec8361c2504ff8d74ced86
Christian Swinehart <drafting@samizdat.co>
2021-09-21 09:03:20 -0400

    use 0.41

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,20 @@ use std::fs;
 use std::io::Write;
 use std::path::Path;
 use skia_safe::textlayout::{FontCollection, ParagraphBuilder, ParagraphStyle, TextStyle};
-use skia_safe::{icu, Rect, Surface, Canvas, FontMgr, Paint, Point, EncodedImageFormat, PictureRecorder, Color, PaintStyle};
+use skia_safe::{icu, Rect, Surface, Canvas, FontMgr, Paint, Point, EncodedImageFormat, PictureRecorder};
 
 static LOREM_IPSUM: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at leo at nulla tincidunt placerat. Proin eget purus augue. Quisque et est ullamcorper, pellentesque felis nec, pulvinar massa. Aliquam imperdiet, nulla ut dictum euismod, purus dui pulvinar risus, eu suscipit elit neque ac est. Nullam eleifend justo quis placerat ultricies. Vestibulum ut elementum velit. Praesent et dolor sit amet purus bibendum mattis. Aliquam erat volutpat.";
 
 fn main() {
-    icu::init();
-
-    let width = 512;
+    let width = 256;
     let height = 64;
-    let mut surface = Surface::new_raster_n32_premul((width * 2, height * 2)).unwrap();
-    let canvas = surface.canvas();
-    let mut paint = Paint::default();
-    paint
-      .set_stroke_miter(10.0)
-      .set_color(Color::BLACK)
-      .set_anti_alias(true)
-      .set_stroke_width(1.0)
-      .set_style(PaintStyle::Fill);
 
+    let mut surface = Surface::new_raster_n32_premul((width * 2, height * 2)).unwrap();
+    let mut canvas = surface.canvas();
     canvas.scale((2.0, 2.0));
+    draw_lorem_ipsum(&mut canvas);
+    save_as(&mut surface, "immediate.png");
+
 
     let mut layer_recorder = PictureRecorder::new();
     let bounds = Rect::from_xywh(0.0, 0.0, width as f32, height as f32);
@@ -30,21 +24,18 @@ fn main() {
         draw_lorem_ipsum(&mut layer);
     }
 
+    let mut surface = Surface::new_raster_n32_premul((width * 2, height * 2)).unwrap();
+    let canvas = surface.canvas();
+    canvas.scale((2.0, 2.0));
     if let Some(pict) = layer_recorder.finish_recording_as_picture(Some(&bounds)){
-        canvas.save();
-        canvas.draw_picture(&pict, None, Some(&paint));
+        canvas.draw_picture(&pict, None, None);
     }
-
-
-    let image = surface.image_snapshot();
-    let data = image.encode_to_data(EncodedImageFormat::PNG).unwrap();
-    let file_path = Path::new("lorem-ipsum.png");
-    let mut file = fs::File::create(file_path).expect("failed to create file");
-    file.write_all(data.as_bytes()).expect("failed to write to file");
+    save_as(&mut surface, "recorded.png");
 }
 
 
 fn draw_lorem_ipsum(canvas: &mut Canvas) {
+    icu::init();
     let mut font_collection = FontCollection::new();
     font_collection.set_default_font_manager(FontMgr::new(), None);
     let paragraph_style = ParagraphStyle::new();
@@ -58,3 +49,10 @@ fn draw_lorem_ipsum(canvas: &mut Canvas) {
     paragraph.paint(canvas, Point::default());
 }
 
+fn save_as(surface: &mut Surface, name: &str){
+    let image = surface.image_snapshot();
+    let data = image.encode_to_data(EncodedImageFormat::PNG).unwrap();
+    let file_path = Path::new(name);
+    let mut file = fs::File::create(file_path).expect("failed to create file");
+    file.write_all(data.as_bytes()).expect("failed to write to file");
+}
