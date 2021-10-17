78e5d7ff7ae4a23549def8f7b56b56d8df1c9acc
Christian Swinehart <drafting@samizdat.co>
2021-10-17 11:33:14 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,6 @@ lazy_static! {
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
 
-  use skia_safe::shaper::icu;
-  icu::init();
-
   // -- Image -------------------------------------------------------------------------------------
 
   cx.export_function("Image_new", image::new)?;
