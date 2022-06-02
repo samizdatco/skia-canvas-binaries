6b36d5042bdd392960735cfa5d91b79b42d237f3
Christian Swinehart <drafting@samizdat.co>
2022-06-02 11:10:37 -0400

    Update testglob.js

--- testglob.js
+++ testglob.js
@@ -5,12 +5,12 @@ const fs = require('fs'),
 TMP = tmp.dirSync().name
 console.log('DIR', TMP)
 
-fs.writeFileSync(`${TMP}/output-01.png`)
-fs.writeFileSync(`${TMP}/output-02.png`)
-fs.writeFileSync(`${TMP}/output-03.png`)
-fs.writeFileSync(`${TMP}/output-04.png`)
-fs.writeFileSync(`${TMP}/output-05.png`)
-fs.writeFileSync(`${TMP}/output-06.png`)
+fs.writeFileSync(`${TMP}/output-01.png`, 'empty')
+fs.writeFileSync(`${TMP}/output-02.png`, 'empty')
+fs.writeFileSync(`${TMP}/output-03.png`, 'empty')
+fs.writeFileSync(`${TMP}/output-04.png`, 'empty')
+fs.writeFileSync(`${TMP}/output-05.png`, 'empty')
+fs.writeFileSync(`${TMP}/output-06.png`, 'empty')
 
 let qmark = glob(`${TMP}/output-0?.png`),
     star = glob(`${TMP}/output-*.png`)
