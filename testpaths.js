6cad1258df21ede73078ac2d3ee78991cc931beb
Christian Swinehart <drafting@samizdat.co>
2025-08-17 01:28:08 -0400

    Update testpaths.js

--- testpaths.js
+++ testpaths.js
@@ -1,27 +1,17 @@
 let {globSync, convertPathToPattern} = require('fast-glob'),
     path = require('path')
 
-/**
- * The device path (\\.\ or \\?\).
- * https://learn.microsoft.com/en-us/dotnet/standard/io/file-path-formats#dos-device-paths
- */
-const DOS_DEVICE_PATH_RE = /^\\\\(?<path>[.?])/;
-/**
- * All backslashes except those escaping special characters.
- * Windows: !()+@{}
- * https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
- */
-const WINDOWS_BACKSLASHES_RE = /\\(?![!()+@[\]{}])/g;
-
 // const ROOT_DIR = __dirname
-const ROOT_DIR = 'D:\\a\\skia-canvas-binaries\\skia-canvas-binaries'.replace()
-        .replace(DOS_DEVICE_PATH_RE, '//$1')
-    		.replaceAll(WINDOWS_BACKSLASHES_RE, '/')
-console.log({ROOT_DIR})
+// const ROOT_DIR = 'D:\\a\\skia-canvas-binaries\\skia-canvas-binaries'.replace()
+//         .replace(/^\\\\(?<path>[.?])/, '//$1') // The device path (\\.\ or \\?\).
+//     		.replaceAll(/\\(?![!()+@[\]{}])/g, '/') // All backslashes except those escaping special characters.
+// console.log({ROOT_DIR})
 const ASSETS_DIR = path.join(__dirname, 'test/assets'),
       FONTS_DIR = path.join(ASSETS_DIR, 'fonts')
 
 function glob(pat){
+  pat = pat.replace(/^\\\\(?<path>[.?])/, '//$1') // The device path (\\.\ or \\?\).
+    .replaceAll(/\\(?![!()+@[\]{}])/g, '/') // All backslashes except those escaping special characters.
   console.log('pattern:', pat)
   console.log(globSync(pat))
 }
