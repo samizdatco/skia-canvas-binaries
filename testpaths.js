a8fcea468d3a119d17dbc8a5e0a11eb229bb7752
Christian Swinehart <drafting@samizdat.co>
2025-08-17 01:16:07 -0400

    update

--- testpaths.js
+++ testpaths.js
@@ -1,6 +1,21 @@
 let {globSync, convertPathToPattern} = require('fast-glob'),
     path = require('path')
 
+/**
+ * The device path (\\.\ or \\?\).
+ * https://learn.microsoft.com/en-us/dotnet/standard/io/file-path-formats#dos-device-paths
+ */
+const DOS_DEVICE_PATH_RE = /^\\\\(?<path>[.?])/;
+/**
+ * All backslashes except those escaping special characters.
+ * Windows: !()+@{}
+ * https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
+ */
+const WINDOWS_BACKSLASHES_RE = /\\(?![!()+@[\]{}])/g;
+
+const ROOT_DIR = __dirname.replace()
+        .replace(DOS_DEVICE_PATH_RE, '//$1')
+    		.replaceAll(WINDOWS_BACKSLASHES_RE, '/')
 
 const ASSETS_DIR = path.join(__dirname, 'test/assets'),
       FONTS_DIR = path.join(ASSETS_DIR, 'fonts')
